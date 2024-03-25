fe72fc86c845d00bf7d7757b46afbfa6b3cf7869
AlexHelmkvist <148632501+AlexHelmkvist@users.noreply.github.com>
2024-03-25 13:18:27 +0100

    Update HelloWorld.java
    Cats and Dogs

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -2,8 +2,8 @@ package helloword;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Cat!");
-        System.out.println("Hello Dog!");
+        System.out.println("Hello Cats!");
+        System.out.println("Hello Dogs!");
 
         System.out.println("Hello World!!");
         System.out.println("Hej San!");
