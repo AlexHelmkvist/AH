3191268ccde9065acd78a867729b4c6a34425932
Alex <alex33376@hotmail.com>
2024-03-25 12:57:12 +0100

    Hello World

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -2,6 +2,6 @@ package helloword;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Dog!");
+        System.out.println("Hello World!");
     }
 }
